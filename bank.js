41b23fbfc19fa451cabc35e6945a3fd5734d7ec1
Alisia-Nadia <sarbalisia@yahoo.com>
2019-11-11 01:18:47 -0800

    Create bank.js

---
+++ bank.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const jwt  = require('jsonwebtoken');
+const jwt_decode = require('jwt-decode');
+
+
+var secret =  'secret'
+
+
+var fs = require('fs')
+var data = fs.readFileSync('data.txt').toString().split("\n")
+
+
+app.get( '/getDetails' , (req,res) => {
+  
+    var decodeToken = jwt.decode(req.query.tokenUser);
+    console.log(decodeToken)
+    var userInfo
+    for(i in data){         
+         if(data[i].split(' ')[0] == decodeToken.substr(0,6)){
+        
+            userInfo = data[i]
+            break
+               
+        }
+    }
+    
+    res.send(userInfo)
+})
+
+app.listen(2223, (err) => {
+    if(err){
+        console.log('Can not listen!')
+        return
+    }
+    
+    console.log('Server listening...')
+ 
+})
